2880a21713f461af59541028e19b8de94cfe13fc
perrineosw <oswald.perrinedu40@gmail.com>
2022-11-10 16:46:28 +0100

    save rognage image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="card">
         <div class="card-img-top">
-            <img src="{{ URL('storage/img/fond_accueil.jpeg') }}" class="image-fond" alt=""/>
+            <img src="{{ URL('storage/img/fond_accueilV2.jpeg') }}" class="image-fond" alt=""/>
         </div>
         <div class="card-img-overlay h-100 text-center d-flex align-items-center justify-content-center">
             <h1><label class="display-2">Bienvenue sur<br>AntiGaspi</label><br><br>Votre site de troc alimentaire anti-gaspi !</h1>
