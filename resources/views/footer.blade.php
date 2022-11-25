151f1daf0481e7a4f81db7d9e238eb9d6500c904
Théodore Diouf <92777850+Tdiouf10@users.noreply.github.com>
2022-11-25 10:02:01 +0100

    :hammer:

--- resources/views/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 px-5 border-top text-black w-100"
-    style="background-color: #b1dfbb">
-    <p class="col-lg-4 col-2 mb-0">© 2022</p>
-    <ul class="nav col-lg-5 col-7 justify-content-end">
-        <li class="nav-item"><a href="{{ url('/nouscontacter')}}" class="nav-link hover-underline-animation">Nous contacter</a></li>
-        <li class="nav-item"><a href="{{ url('/apropos') }}" class="nav-link hover-underline-animation">A Propos</a></li>
-        <li class="nav-item"><a href="#" class="nav-link hover-underline-animation">Mentions Légales</a></li>
-    </ul>
-</footer>
