c5681417bcad11bc11739485295fed63bdef3f76
perrineosw <oswald.perrinedu40@gmail.com>
2022-12-01 16:54:04 +0100

    save annonce detail

--- resources/views/annonces/detail.blade.php
+++ resources/views/annonces/detail.blade.php
@@ -2,26 +2,27 @@
 
 @section('content')
     <div class="container" style="margin-top: 100px;" >
-        <div class="card">
-            <div class="card-img-overlay">
-                <div class="container mt-5">
-                    <div id="results">
-                        <div class="row row-cols-1 row-cols-md-4 g-4">
-                            <div class="col py-4">
-                                <div class="card card_style h-100 shadow-sm">
-                                    <img class="card-img-top" src="{{ asset($annonce->image)}}">
-                                    <div class="card-body text-center">
-                                        <h5 class="card-title">{{ $annonce->title  }}</h5>
-                                        <small> {{ Carbon\Carbon::parse($annonce->created_at)->diffForHumans() }}</small>
-                                        <p class="card-text text-info"> {{ $annonce->localisation }}</p>
-                                        <p class="card-text"> {{ $annonce->description }}</p>
-                                        <p class="card-text"> {{ $annonce->user->name." ". $annonce->user->firstname}}</p>
-                                        <p class="card-text"> 0{{ $annonce->user->telephone }}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div class="card card_style shadow-sm my-auto">
+            <div class="card-body row">
+                <div class="col text-center">
+                    <h2>Informations de l'annonce</h2>
+                    <h5 class="card-title mt-5">{{ $annonce->title  }}</h5>
+                    <small> {{ Carbon\Carbon::parse($annonce->created_at)->diffForHumans() }}</small>
+                    <p class="card-text text-info"> {{ $annonce->localisation }}</p>
+                    <img class="card-img-top my-3" src="{{ asset($annonce->image)}}">
+                    <p class="card-text"> {{ $annonce->description }}</p>
+                </div>
+                <div class="col">
+                    <h2 class="text-center">Annonce déposée par :</h2>
+                    <div class="mt-5 text-center">
+                        <p>Nom : {{ $annonce->user->name }}</p>
+                        <p>Prénom : {{ $annonce->user->firstname }}</p>
+                        <p>Numéro de téléphone : 0{{ $annonce->user->telephone }}</p>
+                        <p>Email : {{ $annonce->user->email }}</p>
                     </div>
+                </div>
+                <div class="text-center mt-5">
+                    <a href="{{ route('annonces.index') }}" class="btn btn-danger"> Retour </a>
                 </div>
             </div>
         </div>
